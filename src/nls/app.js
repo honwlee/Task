86779dc00a8659b724586e21d9f02c5cb2e3e17d
honwlee <lihongwang0618@gmail.com>
2016-01-19 09:52:03 +0800

    init project

---
+++ src/nls/app.js
@@ -0,0 +1,11 @@
+define({
+    root: ({
+        "placeholder": "输入新任务",
+        "add": "添加",
+        "newTask": "新任务",
+        "completed": "已完成",
+        "remaining": "未完成"
+    }),
+    "zh": true,
+    "en": true
+});
---
+++ src/nls/zh/app.js
@@ -0,0 +1,7 @@
+define({
+    "placeholder": "输入新任务",
+    "add": "添加",
+    "newTask": "新任务",
+    "completed": "已完成",
+    "remaining": "未完成"
+});
